3e51a2d8ea42fbe682dc88ca16b011b36bf454d1
Gregor <ggassner@math.uni-koeln.de>
2017-01-19 15:27:42 +0100

    header length correction

--- src/interpolation/changeBasis.f90
+++ src/interpolation/changeBasis.f90
@@ -1,20 +1,18 @@
-!=====================================================================================================================================
+!==================================================================================================================================
 ! Copyright (c) 2016 Gregor Gassner
 ! Copyright (c) 2016 Florian Hindenlang
 ! Copyright (c) 2016 Andrew Winters
 ! Copyright (c) 2010 - 2016  Claus-Dieter Munz (github.com/flexi-framework/flexi)
 !
 ! This file is part of FLUXO (github.com/project-fluxo/fluxo). FLUXO is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
-! or (at your option) any later version.
+! it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 
+! of the License, or (at your option) any later version.
 !
 ! FLUXO is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 ! of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License v3.0 for more details.
 !
 ! You should have received a copy of the GNU General Public License along with FLUXO. If not, see <http://www.gnu.org/licenses/>.
-!=====================================================================================================================================
-
-
+!==================================================================================================================================
 !==================================================================================================================================
 !> Changes a 2D or 3D tensor product polynomial with Lagrange Basis of degree NIn to
 !> 2D or 3D tensor product polynomial of a Lagrange Basis NOut, using two
