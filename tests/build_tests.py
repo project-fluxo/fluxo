e8c0f10cdf0eed2d091f4ff2e1612c092512069f
Florian Hindenlang <florian.hindenlang@ipp.mpg.de>
2017-03-08 19:40:31 +0100

    fixes in python build_tests script, stdout and stderr are parsed. added correct error handling for cmake part.

--- tests/build_tests.py
+++ tests/build_tests.py
@@ -8,71 +8,89 @@ import shutil
 import tempfile
 import time
 import argparse
-import subprocess
 
 
 ########################################################################################################
 
-def buildfluxo( buildopts=None , log = True , log_path="out" , ntail = 0 , mpi_procs = 1):
+def buildfluxo( buildopts=None , project="test" , ntail = 0 , mpi_procs = 1, keepdir=0):
+   # build directory
+   builddir='dir_'+project
+   os.system('rm -rf '+builddir) #destroy directory from previous run
+   os.system('mkdir '+builddir )
+   os.chdir(builddir)
+
    allopts=" "
    for i in range((len(buildopts)/2)) :
       allopts=allopts+" -D"+buildopts[2*i]+"="+buildopts[2*i+1]+" " 
 
    print "===> OPTIONS:"
    print allopts
-   if log :
-      f = open(log_path, 'w')
-      f.write("===> OPTIONS: \n %s \n" % (allopts) )
 
+   log_path="../log."+project
+   logf = open(log_path, 'w')
+   logf.write("===> OPTIONS: \n %s \n" % (allopts) )
+ 
    cmdconfig = "cmake ../../. "+allopts
    print "  "
    print "===> configure..."
-   pc = subprocess.Popen(cmdconfig, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
-   lines = []
-   while pc.poll() is None :
-      line = pc.stdout.readlines()
-      if line :
-         lines.extend(line)
-   if log :
-      for line in lines :
-         f.write(line)
+   os.system(cmdconfig+" 2>stderr 1>stdout")
 
+   stdout=open("stdout",'r').readlines()
+   stderr=open("stderr",'r').readlines()
+   cmake_err= False
+   for line in stderr :
+      logf.write(line)
+      if "ERROR" in line :
+         cmake_err= True
+         sys.stdout.write("%s" % line)
+
+   for line in stdout :
+      logf.write(line)
+
+
+   logf.close()
    if (ntail > 0) :
-      nlines=len(lines)
+      nlines=len(stdout)
       i=0
-      for line in lines :
+      for line in stdout :
          i=i+1
          if ( i > (nlines -ntail) ) :
-            print line,
+            sys.stdout.write("%s" % line)
+   if(cmake_err) :
+      print "===============================================  "
+      print " !!!! ERROR IN CMAKE, NOT FINISHED CORRECTLY!!!!! "
+      print "===============================================  "
+      print "  "
+      return (not cmake_err)
    #MAKE
    print "  "
    print "===> make..."
    cmdmake = ("make -j %d VERBOSE=1" % (mpi_procs) )
-   pm = subprocess.Popen(cmdmake, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
-   lines = []
-   while pm.poll() is None :
-      line = pm.stdout.readlines()
-      if line :
-         lines.extend(line)
-   if log :
-      for line in lines :
-         f.write(line)
-      f.close()
+   os.system(cmdmake+" 2>stderr 1>stdout")
+
+   stdout=open("stdout",'r').readlines()
+   stderr=open("stderr",'r').readlines()
+   
+   logf = open(log_path, 'a')
+   build_success= False
+   for line in stdout :
+      logf.write(line)
+      if "SUCCESS: FLUXO BUILD COMPLETE!" in line :
+         build_success= True
+   logf.close()
    if (ntail > 0) :
-      nlines=len(lines)
+      nlines=len(stdout)
       i=0
-      for line in lines :
+      for line in stdout :
          i=i+1
          if ( i > (nlines -ntail) ) :
-            print line,
-   build_success= False
-   for l in lines[-30:] :
-      if "SUCCESS: FLUXO BUILD COMPLETE!" in l :
-         build_success= True
-      if(build_success) :
-        break
+            sys.stdout.write("%s" % line)
+
+   os.chdir('../')
    print "===============================================  "
    if (build_success) :
+     if(keepdir ==0) :
+        os.system('rm -rf '+builddir) #destroy directory
      print " Build finished sucessfully."
    else :
      print " !!!! PROBLEM WITH BUILD, NOT FINISHED CORRECTLY!!!!! "
@@ -94,14 +112,11 @@ parser.add_argument('-withmpi', type=int, default=1, help="(1) compile with mpi 
                                                           "(0) compile without mpi  ")
 parser.add_argument('-buildhdf5', type=int, default=1, help="(1) build hdf5 locally (default),\n"
                                                             "(0) use external hdf5 (modules)")
-parser.add_argument('-eqnsys', type=int, default=0, help="equation system to be used: \n"
-                                                         "(0) linearscalaradvection (default) ,\n"
-                                                         "(1) maxwell\n" )
+parser.add_argument('-keepdir', type=int, default=1, help="(1) keep all build directories (default),\n" 
+                                                          "(0) delete sucessfull build directories")
 
 args = parser.parse_args()
 
-os.system('mkdir -p dir_build_test')
-os.chdir('dir_build_test')
 
 if(args.withmpi == 0) :
   MPIOPT="OFF"
@@ -113,22 +128,15 @@ if(args.buildhdf5 == 0) :
 else :
   HDF5OPT="ON"
 
-if(args.eqnsys == 0) :
-  EQNSYS="linearscalaradvection"
-elif(args.eqnsys == 1) :
-  EQNSYS="maxwell"
-else :
-  EQNSYS="linearscalaradvection"
 
 builderr= "_"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-outfile="../log.build_test_1"
 
 options=[ "CMAKE_BUILD_TYPE"       ,"Release"
          ,"FLUXO_BUILD_MPI"        ,MPIOPT
          ,"FLUXO_BUILD_HDF5"       ,HDF5OPT
-         ,"FLUXO_EQNSYSNAME"       ,EQNSYS
+         ,"FLUXO_EQNSYSNAME"       ,"linearscalaradvection"
          ,"FLUXO_DISCTYPE"         ,"1"
          ,"FLUXO_DISC_NODETYPE"    ,"GAUSS-LOBATTO"
          ,"FLUXO_PARABOLIC"        ,"ON"
@@ -136,19 +144,19 @@ options=[ "CMAKE_BUILD_TYPE"       ,"Release"
          ,"FLUXO_TESTCASE"         ,"default" 
         ]
 
-stat = buildfluxo(buildopts=options,log= True, log_path=outfile,\
+pname="build_test_1"
+stat = buildfluxo(buildopts=options, project=pname,\
                               ntail = args.ntail ,\
-                              mpi_procs = args.procs )
+                              mpi_procs = args.procs , keepdir=args.keepdir )
 if(not stat) :
-  builderr= builderr+" 1"
+  builderr= builderr+" "+pname
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  
-outfile="../log.build_test_2"
 
 options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
          ,"FLUXO_BUILD_MPI"        ,MPIOPT
          ,"FLUXO_BUILD_HDF5"       ,HDF5OPT
-         ,"FLUXO_EQNSYSNAME"       ,EQNSYS
+         ,"FLUXO_EQNSYSNAME"       ,"linearscalaradvection"
          ,"FLUXO_DISCTYPE"         ,"1"
          ,"FLUXO_DISC_NODETYPE"    ,"GAUSS-LOBATTO"
          ,"FLUXO_PARABOLIC"        ,"ON"
@@ -156,19 +164,19 @@ options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
          ,"FLUXO_TESTCASE"         ,"default" 
         ]
 
-stat = buildfluxo(buildopts=options,log= True, log_path=outfile,\
+pname="build_test_2"
+stat = buildfluxo(buildopts=options, project=pname,\
                               ntail = args.ntail ,\
-                              mpi_procs = args.procs )
+                              mpi_procs = args.procs , keepdir=args.keepdir )
 if(not stat) :
-  builderr= builderr+" 2"
+  builderr= builderr+" "+pname
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  
-outfile="../log.build_test_3"
 
 options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
          ,"FLUXO_BUILD_MPI"        ,MPIOPT
          ,"FLUXO_BUILD_HDF5"       ,HDF5OPT
-         ,"FLUXO_EQNSYSNAME"       ,EQNSYS
+         ,"FLUXO_EQNSYSNAME"       ,"linearscalaradvection"
          ,"FLUXO_DISCTYPE"         ,"1"
          ,"FLUXO_DISC_NODETYPE"    ,"GAUSS"
          ,"FLUXO_PARABOLIC"        ,"ON"
@@ -176,19 +184,19 @@ options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
          ,"FLUXO_TESTCASE"         ,"default" 
         ]
 
-stat = buildfluxo(buildopts=options,log= True, log_path=outfile,\
+pname="build_test_3"
+stat = buildfluxo(buildopts=options, project=pname,\
                               ntail = args.ntail ,\
-                              mpi_procs = args.procs )
+                              mpi_procs = args.procs , keepdir=args.keepdir )
 if(not stat) :
-  builderr= builderr+" 3"
+  builderr= builderr+" "+pname
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  
-outfile="../log.build_test_4"
 
 options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
          ,"FLUXO_BUILD_MPI"        ,MPIOPT
          ,"FLUXO_BUILD_HDF5"       ,HDF5OPT
-         ,"FLUXO_EQNSYSNAME"       ,EQNSYS
+         ,"FLUXO_EQNSYSNAME"       ,"linearscalaradvection"
          ,"FLUXO_DISCTYPE"         ,"2"
          ,"FLUXO_DISC_NODETYPE"    ,"GAUSS-LOBATTO"
          ,"FLUXO_PARABOLIC"        ,"ON"
@@ -196,31 +204,15 @@ options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
          ,"FLUXO_TESTCASE"         ,"default" 
         ]
 
-stat = buildfluxo(buildopts=options,log= True, log_path=outfile,\
+pname="build_test_4"
+stat = buildfluxo(buildopts=options, project=pname,\
                               ntail = args.ntail ,\
-                              mpi_procs = args.procs )
+                              mpi_procs = args.procs , keepdir=args.keepdir )
 if(not stat) :
-  builderr= builderr+" 4"
+  builderr= builderr+" "+pname
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-outfile="../log.build_test_5"
 
-options=[ "CMAKE_BUILD_TYPE"       ,"Debug"
-         ,"FLUXO_BUILD_MPI"        ,MPIOPT
-         ,"FLUXO_BUILD_HDF5"       ,HDF5OPT
-         ,"FLUXO_EQNSYSNAME"       ,EQNSYS
-         ,"FLUXO_DISCTYPE"         ,"2"
-         ,"FLUXO_DISC_NODETYPE"    ,"GAUSS"
-         ,"FLUXO_PARABOLIC"        ,"OFF"
-         ,"FLUXO_TESTCASE"         ,"default" 
-        ]
-
-stat = buildfluxo(buildopts=options,log= True, log_path=outfile,\
-                              ntail = args.ntail ,\
-                              mpi_procs = args.procs )
-if(not stat) :
-  builderr= builderr+" 4"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if(len(builderr) > 1 ) :
   print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
