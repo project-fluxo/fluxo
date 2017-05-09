156b78d21da304d0629ee8cbb95b6f4ffe97a1c6
Florian Hindenlang <florian.hindenlang@ipp.mpg.de>
2017-05-09 16:24:13 +0200

    added copyright to python scripts and ini files. defined a global cache only.

--- tests/build_tests.py
+++ tests/build_tests.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#==================================================================================================================================
+# Copyright (c) 2016 - 2017 Gregor Gassner
+# Copyright (c) 2016 - 2017 Florian Hindenlang
+# Copyright (c) 2016 - 2017 Andrew Winters
+#
+# This file is part of FLUXO (github.com/project-fluxo/fluxo). FLUXO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 
+# of the License, or (at your option) any later version.
+#
+# FLUXO is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License v3.0 for more details.
+#
+# You should have received a copy of the GNU General Public License along with FLUXO. If not, see <http://www.gnu.org/licenses/>.
+#==================================================================================================================================
 
 import os
 import sys
