9808af3b665ccb01d9440735be18ea88fd6b7617
Florian Hindenlang <florian.hindenlang@ipp.mpg.de>
2018-07-07 22:22:45 +0200

    modification: using optimized flux_average volint (only volflux=0,2,10 working) and called as buffer in Ucomm, and visc volint called as buffer for gradU comm.

--- src/dg/dg.f90
+++ src/dg/dg.f90
@@ -257,10 +257,14 @@ USE MOD_FillMortar          ,ONLY: U_Mortar,Flux_Mortar
 USE MOD_Mesh_Vars           ,ONLY: sJ
 USE MOD_DG_Vars             ,ONLY: nTotalU,nTotal_IP
 USE MOD_ProlongToFace       ,ONLY: ProlongToFace
+#if PP_DiscType==1
 USE MOD_VolInt              ,ONLY: VolInt
-#if PP_DiscType==2
+#elif PP_DiscType==2
 USE MOD_Flux_Average        ,ONLY: VolInt_splitForm_eqn
-#endif
+#if PARABOLIC
+USE MOD_VolInt              ,ONLY: VolInt_visc
+#endif /*PARABOLIC*/
+#endif /*PP_DiscType==2*/
 USE MOD_GetBoundaryFlux     ,ONLY: GetBoundaryFlux
 USE MOD_FillFlux            ,ONLY: FillFlux
 USE MOD_SurfInt             ,ONLY: SurfInt
@@ -305,6 +309,9 @@ CALL StartSendMPIData(U_slave,DataSizeSide,FirstSlaveSide,LastSlaveSide, &
 !!write(*,*)'u in dgtimederivative', U
 !!write(*,*)'u_slave before prolong', u_slave
 !!write(*,*)'u_master before prolong', u_master
+#if PP_DiscType==2
+CALL VolInt_splitForm_eqn(Ut)
+#endif
 
 CALL ProlongToFace(U,U_master,U_slave,doMPISides=.FALSE.)
 CALL U_Mortar(U_master,U_slave,doMPISides=.FALSE.)
@@ -326,8 +333,9 @@ CALL Lifting(tIn)
 #if PP_DiscType==1
 CALL VolInt(Ut)
 #elif PP_DiscType==2
-CALL VolInt(Ut)
-!CALL VolInt_splitForm_eqn(Ut)
+#if PARABOLIC
+CALL VolInt_visc(Ut)
+#endif /*PARABOLIC*/
 #endif
 
 
