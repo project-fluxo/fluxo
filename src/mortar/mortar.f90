1687ab020cecbc7db97547a283684b1375028a70
Alexander Astanin <aastanin@math.uni-koeln.de>
2020-09-09 14:22:22 +0200

    User projection instead of collocation

--- src/mortar/mortar.f90
+++ src/mortar/mortar.f90
@@ -30,8 +30,8 @@ INTERFACE MortarBasis_BigToSmall
 END INTERFACE
 
 INTERFACE MortarBasis_SmallToBig
-  MODULE PROCEDURE MortarBasis_SmallToBig_Collocation
-  !MODULE PROCEDURE MortarBasis_SmallToBig_Projection
+  ! MODULE PROCEDURE MortarBasis_SmallToBig_Collocation
+  MODULE PROCEDURE MortarBasis_SmallToBig_Projection
 END INTERFACE
 
 INTERFACE FinalizeMortar
